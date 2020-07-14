cb370642ee0135b403399399f2f9d7d4ab075a6c
Armando Tapia <rodney.tapia@epn.edu.ec>
2020-07-14 18:33:43 -0500

    [ADD] some hooks for reuse them

---
+++ 06-repo-hooks/useForms.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useForms = (initialState = {}) => {
+  // Se puede agregar reglas de validacion
+  // se puede colocar una bandera para decir cuando el formulario es correcto o cuando no
+  // etc
+  const [values, setValues] = useState(initialState);
+
+  const reset = () => {
+    setValues(initialState);
+  };
+
+  const hadleInputChange = ({ target }) => {
+    setValues({ ...values, [target.name]: target.value });
+  };
+
+  return [values, hadleInputChange, reset];
+};
